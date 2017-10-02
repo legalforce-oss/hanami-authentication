0e5f7101304979164904337ae3ab51be05bcbaaa
Yuji Ueki <unhappychoice@gmail.com>
2017-10-02 16:39:04 +0900

    Remove unnecessary spec

--- spec/hanami/auth_spec.rb
+++ spec/hanami/auth_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Hanami::Auth do
   it 'has a version number' do
     expect(Hanami::Auth::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
