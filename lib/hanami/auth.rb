daf1690e1c19fd8ecc9080555282fe1cb51e6e76
Yuji Ueki <unhappychoice@gmail.com>
2017-10-02 16:31:35 +0900

    Implement auth module

--- lib/hanami/auth.rb
+++ lib/hanami/auth.rb
@@ -1,7 +1,61 @@
-require "hanami/auth/version"
+require 'hanami/auth/version'
+require 'bcrypt'
 
 module Hanami
   module Auth
-    # Your code goes here...
+
+    private
+
+    def login(user, remember_me: false, expire_seconds: 1 * 60 * 60)
+      return if authenticated?
+      session[:current_user] = user
+      session[:session_created_at] = Time.now + expire_seconds
+      session[:remember_me] = remember_me
+    end
+
+    def logout
+      return unless authenticated?
+      session[:current_user] = nil
+      session[:session_created_at] = nil
+      session[:remember_me] = nil
+    end
+
+    def create_password(password)
+      BCrypt::Password.create(password)
+    end
+
+    def match_password?(user, password)
+      user && BCrypt::Password.new(user.password_digest) == password
+    end
+
+    def current_user
+      @current_user ||= session[:current_user]
+    end
+
+    def authenticate!
+      if session_expired? || !authenticated?
+        logout
+        halt 401
+      end
+    end
+
+    def authenticate
+      if session_expired?
+        logout
+        flash[:error] = 'セッションの有効期限が切れました'
+        redirect_to routes.root_path
+      elsif !authenticated?
+        flash[:error] = 'ログインしてください'
+        redirect_to routes.root_path
+      end
+    end
+
+    def authenticated?
+      !!session[:current_user]
+    end
+
+    def session_expired?
+      !session[:remember_me] && session[:session_expired_at] && session[:session_expired_at] < Time.now
+    end
   end
 end
--- lib/hanami/auth/version.rb
+++ lib/hanami/auth/version.rb
@@ -1,5 +1,5 @@
 module Hanami
   module Auth
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'
   end
 end
